33d9a97c3f5113863eb32a8f80435044109f88e6
David Arnold <david.arnold@iohk.io>
2023-08-31 14:03:42 +0200

    chore: implement fix on minimal example
    with debug tracing, we can see the change in behaviour
    
    before: (wrong)
    ```
    trace: candidate directory: /nix/store/...-source/example/foo1
    trace: 	recurse on prefix: /nix/store/...-source/example/foo10
    ```
    
    after: (good)
    ```
    trace: candidate directory: /nix/store/...-source/example/foo1
    ```
    
    Recursion into the folder does not happen anymore

--- example/flake.nix
+++ example/flake.nix
@@ -5,27 +5,11 @@
   # inputs.incl.url = "github:divnix/incl";
   inputs.incl.url = "path:../.";
 
-  inputs.hive.url = "github:divnix/hive";
-
-  outputs = {
-    hive,
-    incl,
-    self,
-    ...
-  } @ inputs:
-    hive.growOn {
-      inherit inputs;
-
-      cellsFrom = incl ./combs ["comb10"];
-      cellBlocks = with hive.blockTypes; [
-        diskoConfigurations
-      ];
-    }
-    {diskoConfigurations = hive.collect self "diskoConfigurations";}
-    {
-      filteredSource = (incl // {debug = true;}) ./. [
-        ./README.md
-        ./folder/other # and all below
-      ];
-    };
+  outputs = {incl, self}: {
+    filteredSource = (incl // {debug = true;}) ./. [
+      ./README.md
+      ./folder/other # and all below
+      "foo10" # but not foo1
+    ];
+  };
 }
