f4835d12011882bd7f534d6d5c68bb3524f001b1
whs <hswongac@gmail.com>
2023-08-31 11:14:21 +0800

    Fix include extra paths and add example

--- example/flake.nix
+++ example/flake.nix
@@ -5,10 +5,27 @@
   # inputs.incl.url = "github:divnix/incl";
   inputs.incl.url = "path:../.";
 
-  outputs = { incl, self }: {
-    filteredSource = (incl // {debug = true;}) ./. [
-      ./README.md
-      ./folder/other # and all below
-    ];
-  };
+  inputs.hive.url = "github:divnix/hive";
+
+  outputs = {
+    hive,
+    incl,
+    self,
+    ...
+  } @ inputs:
+    hive.growOn {
+      inherit inputs;
+
+      cellsFrom = incl ./combs ["comb10"];
+      cellBlocks = with hive.blockTypes; [
+        diskoConfigurations
+      ];
+    }
+    {diskoConfigurations = hive.collect self "diskoConfigurations";}
+    {
+      filteredSource = (incl // {debug = true;}) ./. [
+        ./README.md
+        ./folder/other # and all below
+      ];
+    };
 }
